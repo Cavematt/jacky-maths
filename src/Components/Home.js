aacdd96b1df346832a3605cf29adcbbffd147ccc
Matt Potts <61755051+Cavematt@users.noreply.github.com>
2021-04-18 13:19:30 +0100

    update for client

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,17 +4,17 @@ import "./Home.css"
 function Home() {
 
     return (
-        <div>
+        <div className="allText">
             <div>
                 <h3>Do you or your children need some help with maths?</h3>
             </div>
             <div>
-                <p>My name is Jacky and I offer 1 to 1 maths tuition at affordable prices. 
-Most parents agree that fully tailored 1 to 1 teaching is extremely efficient. I craft the lessons to ensure your child benefits and becomes more confident in maths and  to help them reach their potential. 
-Or alternatively as an adult you regret not having a secure maths foundation in order to help your child with homework. I can help with gaps and confidence in areas needed with either regular or one-off tuition. 
-I offer lessons throughout Thurso in person or anywhere online. 
-All levels of maths are covered from P3 to Higher. 
-</p>
+                <p>My name is Jacky and I offer 1 to 1 maths tuition at affordable prices.</p> 
+                <p>Most parents agree that fully tailored 1 to 1 teaching is extremely efficient. I craft the lessons to ensure your child benefits and becomes more confident in maths and  to help them reach their potential.</p>
+                <p>Or alternatively as an adult you regret not having a secure maths foundation in order to help your child with homework. I can help with gaps and confidence in areas needed with either regular or one-off tuition.</p> 
+                <p>I offer lessons throughout Thurso in person or anywhere online.</p>
+                <p> All levels of maths are covered from P3 to Higher. 
+                </p>
             </div>
         </div>
     )
